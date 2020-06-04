233c8e3b5977f0150c3ff7bf46207e5884914959
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-06-04 10:27:55 +0530

    export csv

--- weatherscrapper.py
+++ weatherscrapper.py
@@ -1,18 +1,25 @@
 import requests
 from bs4 import  BeautifulSoup
+import pandas as pd
 
 page = requests.get("https://forecast.weather.gov/MapClick.php?lat=47.86127000000005&lon=-94.96922999999998#.Xths_p4zZTY")
 soup = BeautifulSoup(page.content, 'html.parser')
 week = soup.find(id='seven-day-forecast-body')
-#print(week)
 
 items = week.find_all(class_='tombstone-container')
-#print(items[0])
 
 period_names = [item.find(class_='period-name').get_text() for item in items]
 short_descriptions = [item.find(class_='short-desc').get_text() for item in items]
 temperatures = [item.find(class_='temp').get_text() for item in items]
 
-print(period_names)
-print(short_descriptions)
-print(temperatures)
+weather_stuff = pd.DataFrame(
+    {
+        'periods': period_names,
+        'short_description': short_descriptions,
+        'temperature': temperatures,
+    }
+)
+
+print(weather_stuff)
+
+weather_stuff.to_csv('result.csv')
