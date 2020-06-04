e02ac57ee330e3c1b516c43ebf1c643c1e9ff1a2
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-06-04 10:13:56 +0530

    get  all values to variable lists

--- weatherscrapper.py
+++ weatherscrapper.py
@@ -6,4 +6,13 @@ soup = BeautifulSoup(page.content, 'html.parser')
 week = soup.find(id='seven-day-forecast-body')
 #print(week)
 
-print(week.find_all(class_='tombstone-container'))
+items = week.find_all(class_='tombstone-container')
+#print(items[0])
+
+period_names = [item.find(class_='period-name').get_text() for item in items]
+short_descriptions = [item.find(class_='short-desc').get_text() for item in items]
+temperatures = [item.find(class_='temp').get_text() for item in items]
+
+print(period_names)
+print(short_descriptions)
+print(temperatures)
